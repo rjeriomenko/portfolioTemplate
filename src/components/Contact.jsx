d5d8a6bb71ed0bdb3eb3b5457439d4f98cdde6b2
rjeriomenko <rokas@jerio.me>
2023-10-06 07:25:38 -0400

    Add keys to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,19 +26,19 @@ const Contact = () => {
     setLoading(true);
 
     // sign up on emailjs.com (select the gmail service and connect your account).
-    //click on create a new template then click on save.
+    // click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_5l16vwz', // paste your ServiceID here (you'll get one when your service is created).
+        'template_tz8w9e1', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Rokas Jeriomenko', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'rokas@jerio.me', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        '9FoYkOE3H7qW8TUEZ' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
