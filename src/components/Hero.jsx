2eb9b2cdee703ba1763ff9fc51727bef0cc8c58c
rjeriomenko <rokas@jerio.me>
2023-10-06 04:24:33 -0400

    Copy template and change splash

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap } from '../assets';
+import { rokas, bwmap, worldmap } from '../assets';
 
 const Hero = () => {
   return (
@@ -42,12 +42,12 @@ const Hero = () => {
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                Rokas
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+              Full stack web dev. <br className="sm:block hidden" />
+              Part time game dev.
             </p>
           </div>
           <div
@@ -80,14 +80,13 @@ const Hero = () => {
           </a>
         </div>
 
-        {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
           <img
-            className="absolute bottom-0 ml-[50vw] 
-            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
+            className="absolute bottom-[-140px] ml-[35vw] 
+            lg:ml-[60vw] md:ml-[45vw] xmd:ml-[45vw] 2xl:ml-[68vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
+            src={rokas}
+            alt="Rokas Jeriomenko"
           />
         </div>
       </section>
